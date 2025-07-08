8d7a923b61180610258a65b09f847ddfff6f7ca8
achyutem <achyutem.ad@gmail.com>
2025-07-08 16:27:20 +0530

    updated task route

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -4,10 +4,17 @@ const {
   getTasks,
   createTask,
   updateTaskStatus,
+  updateTask,
 } = require("../controllers/taskController");
 const { protect } = require("../middleware/authMiddleware");
 
+// Route to get all tasks and create a new task
 router.route("/").get(protect, getTasks).post(protect, createTask);
+
+// Route to update a task's main details
+router.route("/:id").put(protect, updateTask);
+
+// Route to update only a task's status
 router.route("/:id/status").patch(protect, updateTaskStatus);
 
 module.exports = router;
