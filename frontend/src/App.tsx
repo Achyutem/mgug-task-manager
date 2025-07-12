28b2e0b29cd5fc658ef73145e3f17a588df2c2af
achyutem <achyutem.ad@gmail.com>
2025-07-12 16:51:48 +0530

    wrap in theme context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,24 @@ import PrivateRoute from "./layout/PrivateRoute";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { ThemeProvider } from "./context/themeProvider";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
-    </Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
