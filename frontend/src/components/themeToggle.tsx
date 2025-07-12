2e98c6c229510531e6ca4bf8d78651c7f9dac6a9
achyutem <achyutem.ad@gmail.com>
2025-07-12 16:52:27 +0530

    theme toggle button

---
+++ frontend/src/components/themeToggle.tsx
@@ -0,0 +1,27 @@
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/context/themeProvider";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={toggleTheme}
+      className="bg-background"
+    >
+      {theme === "light" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
