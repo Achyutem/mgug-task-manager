fe1b2eeeb4750e38b64c9aaa863a11eb876399c5
achyutem <achyutem.ad@gmail.com>
2025-07-16 11:15:53 +0530

    types export

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface User {
   id: number;
   name: string;
@@ -17,4 +18,23 @@ export interface Task {
   created_at: string;
   assigner_name: string;
   assignee_name: string;
+}
+
+export interface FilterValues {
+  search: string;
+  status: string;
+  priority: string;
+}
+
+export interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+  login: (email: string, password: string) => Promise<any>;
+  register: (name: string, email: string, password: string) => Promise<any>;
+  logout: () => void;
+}
+
+export interface DecodedToken {
+  id: number;
+  exp: number;
 }
